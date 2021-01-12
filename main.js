c461af74ae7f94e4c39a75ccc8390909fcfd5f68
RoblKyogre <42159204+RoblKyogre@users.noreply.github.com>
2021-01-12 01:16:35 -0800

    Move auto-update script to app.on('ready')

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
 const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
 
-const server = "https://hazel.roblkyogre.vercel.app"
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`
-
-autoUpdater.setFeedURL(feed)
-
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 60000)
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
-
-autoUpdater.on('error', message => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
-
 if (require('electron-squirrel-startup')) return app.quit();
 
 function createWindow () {
@@ -40,8 +12,6 @@ function createWindow () {
   })
 
   mainWindow.loadFile('index.html');
-
-  autoUpdater.checkForUpdates()
 
 }
 
@@ -57,4 +27,47 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
+})
+
+app.on('ready', () => {
+
+  const server = "https://hazel.roblkyogre.vercel.app"
+  const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+  autoUpdater.setFeedURL(feed);
+
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000)
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+
+    console.log('Update Downloaded!')
+    console.log(releaseName)
+    console.log(releaseNotes)
+
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
+
+  autoUpdater.on('checking-for-update', () => console.log('Checking for Updates...'))
+  autoUpdater.on('update-available', () => console.log('Update Available'))
+  autoUpdater.on('update-not-available', () => console.log('No Updates Available'))
+
+  autoUpdater.checkForUpdates()
+
 })
