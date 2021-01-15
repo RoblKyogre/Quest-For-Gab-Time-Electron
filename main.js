42f18e1fb226f67b8d5ca764b3cd27719ef702cd
RoblKyogre <42159204+RoblKyogre@users.noreply.github.com>
2021-01-15 03:04:25 -0800

    Prevent app from updating in dev environments and change update alert to notification

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
+const { app, BrowserWindow, autoUpdater, dialog, Notification } = require('electron')
+const isDev = require('electron-is-dev');
 
 if (require('electron-squirrel-startup')) return app.quit();
 
@@ -13,6 +14,65 @@ function createWindow () {
 
   mainWindow.loadFile('index.html');
 
+}
+
+
+function showNotification (title, body) {
+  const notification = {
+    title: title,
+    body: body
+  }
+  new Notification(notification).show()
+}
+
+function setupUpdates () {
+  if (!isDev) {
+    const server = "https://hazel.roblkyogre.vercel.app"
+    const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+    autoUpdater.setFeedURL(feed);
+
+    setInterval(() => {
+      autoUpdater.checkForUpdates()
+    }, 60000)
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+
+      console.log('Update Downloaded!')
+      console.log(releaseName)
+      console.log(releaseNotes)
+
+      showNotification(
+        "Application Update",
+        "This application has been updated to " + releaseName
+        + ". Restart the application to apply the updates."
+      )
+      /*
+      const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+      }
+
+      dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+      })
+      */
+    })
+
+    autoUpdater.on('error', message => {
+      console.error('There was a problem updating the application')
+      console.error(message)
+    })
+
+    autoUpdater.on('checking-for-update', () => console.log('Checking for Updates...', autoUpdater.getFeedURL()))
+    autoUpdater.on('update-available', () => console.log('Update Available'))
+    autoUpdater.on('update-not-available', () => console.log('No Updates Available'))
+
+    autoUpdater.checkForUpdates()
+  }
 }
 
 app.whenReady().then(createWindow)
@@ -30,44 +90,5 @@ app.on('activate', () => {
 })
 
 app.on('ready', () => {
-
-  const server = "https://hazel.roblkyogre.vercel.app"
-  const feed = `${server}/update/${process.platform}/${app.getVersion()}`
-
-  autoUpdater.setFeedURL(feed);
-
-  setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 60000)
-
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-
-    console.log('Update Downloaded!')
-    console.log(releaseName)
-    console.log(releaseNotes)
-
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-    }
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      if (returnValue.response === 0) autoUpdater.quitAndInstall()
-    })
-  })
-
-  autoUpdater.on('error', message => {
-    console.error('There was a problem updating the application')
-    console.error(message)
-  })
-
-  autoUpdater.on('checking-for-update', () => console.log('Checking for Updates...', autoUpdater.getFeedURL()))
-  autoUpdater.on('update-available', () => console.log('Update Available'))
-  autoUpdater.on('update-not-available', () => console.log('No Updates Available'))
-
-  autoUpdater.checkForUpdates()
-
+   setupUpdates()
 })
